63bc9248217f6cfc8d09399025b06fdcb8243573
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2025-12-23 20:49:26 +0900

    add

--- kitty.plugin.zsh
+++ kitty.plugin.zsh
@@ -14,3 +14,37 @@ if [[ "$TERM" == 'xterm-kitty' ]]; then
   # Change the colour theme
   alias kitty-theme="kitty +kitten themes"
 fi
+
+#####################################################
+# Kitty 独自plugin for oh-my-zsh                        #
+#####################################################
+
+
+klaunch() {
+  emulate -L zsh
+  if (( $# == 0 )); then
+    print -u2 "usage: klaunch [launch-opts...] <command> [args...]"
+    return 2
+  fi
+
+  local -a argv; argv=("$@")
+  local has_type=0 has_cwd=0
+
+  local i=1
+  while (( i <= $#argv )); do
+    case ${argv[i]} in
+      --type=*) has_type=1 ;;
+      --type)   has_type=1; (( i++ )) ;;  # 値を1つ消費
+      --cwd=*)  has_cwd=1 ;;
+      --cwd)    has_cwd=1; (( i++ )) ;;
+    esac
+    (( i++ ))
+  done
+
+  local -a base
+  (( !has_type )) && base+=(--type=os-window)
+  (( !has_cwd  )) && base+=(--cwd=current)
+
+  command kitten @ launch "${base[@]}" "${argv[@]}"
+}
+
